14651276d8e8896a671802c8911aa369bf8258f3
Tales Peres <tales.oliveira@alunos.fho.edu.br>
2020-10-28 11:36:14 -0300

    exercicio 10

---
+++ cap03/ex10.js
@@ -0,0 +1,6 @@
+/*
+Exercício 10 - E tem alguma diferença?
+Diga, em poucas palavras, qual a diferença entre os métodos auxiliares `forEach` e `map`.
+--------------------------------------------------------------------------------------------
+O forEach() é mais usado quando você quer fazer algo com os dados do array, já o map() é mais indicado para alterar os dados do array e retornar essas alterações como um novo array.
+*/
